edaea3041eb975370c400a7a39ecf84da3f9a752
pmcintyre3 <pmcin3@uga.edu>
2015-01-28 02:28:14 -0500

    FINALLY got the parallax and the player character to become visible. Working on incorporating enemies now.

--- char.js
+++ char.js
@@ -1,15 +1,17 @@
-var playerShip = new Image();
-var pCanvas = document.getElementById();
-playerShip.src = "sprites/PlayerShip.png";
-
-function sprite (c) {
-
-    var character = {};
-    
-    character.context = c.context;
-    character.width = c.width;
-    character.height = c.height;
-    character.image = c.image;
-
-    return character;
-}
+// Simple sprite class
+function Sprite(args) {
+    this.image = new Image();
+    this.image.src = args.imgSrc;
+    this.x = args.x;
+    this.y = args.y;
+    this.imgFrames = args.frames || 1;
+    this.frame = 0;
+    // Draw the sprite to the screen
+    this.Draw = function() {
+        ctx.globalAlpha = 1;
+        ctx.drawImage(this.image, this.frame, 0, args.spw,
+            args.spw, this.x, this.y, args.spw, args.spw);
+        this.frame += PLAYER_CHAR_WIDTH;
+        if( this.frame >= this.image.width ) this.frame = 0;
+    };
+};
